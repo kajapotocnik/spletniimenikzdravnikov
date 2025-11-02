f5caf3749479935248e19eccceaee818ed745223
kajapotocnik <kaja.potocnik1@student.um.si>
2025-11-02 11:18:59 +0100

    start

---
+++ povezava.php
@@ -0,0 +1,14 @@
+<?php
+$streznik = "localhost:3306";
+$uporabnik = "root";
+$geslo = "sladoled";
+$baza = "spletniimenikzdravnikov";
+
+$conn = new mysqli($streznik, $uporabnik, $geslo, $baza);
+
+if ($conn->connect_error) {
+    die("Povezava ni uspela: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
